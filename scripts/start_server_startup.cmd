e2ff09dadfdc1a88b26efdad1d4f68f057cc82a9
Luizfel99 <luizfelipesantini99@gmail.com>
2025-10-02 13:08:51 -0700

    chore: add startup cmd to start server on user login

---
+++ scripts/start_server_startup.cmd
@@ -0,0 +1,3 @@
+@echo off
+REM Start the static server on user logon (hidden PowerShell window)
+powershell -NoProfile -WindowStyle Hidden -ExecutionPolicy Bypass -File "C:\Users\luizf\OneDrive\Desktop\plataforma-ong\scripts\start_server_port3000.ps1"
